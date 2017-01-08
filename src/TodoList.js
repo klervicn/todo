0f49c101b359ebdebd3a3a9e514e3958c73411f9
klervicn <corbel.klervi@gmail.com>
2017-01-08 15:30:37 +0100

    Suppr element

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,90 +1,95 @@
-var React=require('react');
+var React = require('react')
 
 /* List elements */
 var TodoElements = React.createClass({
-  render : function(){
-
+  render: function () {
     /* Get item list */
-    var itemsEntries = this.props.entries;
-    var remove = this.props.remove;
+    var itemsEntries = this.props.entries
+    var remove = this.props.remove
 
+    function createTasks (item) {
+      function removeThisItem () {
+        remove(item.key)
+      }
 
-    function createTasks(item) {
-
-      return <li key={item.key} onClick={remove}>{item.text}</li>
+      return <li key={item.key} onClick={removeThisItem}>{item.text}</li>
     }
 
-    /* Stock list of <li>*/
-    var listItems = itemsEntries.map(createTasks);
+    /* Stock list of <li> */
+    var listItems = itemsEntries.map(createTasks)
     return (
       <ul className='List'>
-      {listItems}
+        {listItems}
       </ul>
-    );
+    )
   }
-});
+})
 
 /* List container */
-var TodoList=React.createClass({
+var TodoList = React.createClass({
   /* Task list & temporary input value typed */
-  getInitialState: function(){
-      return {items : [],
-              inputValue : ''} ;
-    },
+  getInitialState: function () {
+    return {items: [],
+      inputValue: ''}
+  },
 
   /* Update input value (state) while typing */
-  updateInputValue : function(evt){
+  updateInputValue: function (evt) {
     this.setState({
-       inputValue: evt.target.value
-     });
+      inputValue: evt.target.value
+    })
   },
 
   /* Get item list, add input to local item list (with id), maj item list */
-  addItem : function(e){
-    var itemArray = this.state.items;
+  addItem: function (e) {
+    e.preventDefault()
+    var itemArray = this.state.items
 
     itemArray.push(
       {
         text: this.state.inputValue,
-        key : Date.now()
+        key: Date.now()
       }
-    );
+    )
 
     this.setState({
-      items : itemArray,
-      inputValue :''
-    });
+      items: itemArray,
+      inputValue: ''
+    })
   },
 
-  removeItem : function(){
-    var index = 1 /*this.state.items.indexOf([item.text, item.key])*/;
-    this.state.items.splice(index, 1);
+  removeItem: function (keyToRemove) {
+    var nextItems = []
+
+    for (var item of this.state.items) {
+      if (item.key !== keyToRemove) {
+        nextItems.push(item)
+      }
+    };
 
     this.setState({
-      items : this.state.items
+      items: nextItems
 
-    });
-
+    })
   },
 
   /* Display */
   render: function () {
     return (
-      <div  className='TodoList'>
-        <div  className='TodoForm'>
-          <form>
+      <div className='TodoList'>
+        <div className='TodoForm'>
+          <form onSubmit={this.addItem}>
             <label>
-              <input type="text" name="element" placeholder="Enter a new task" value={this.state.inputValue} onChange={this.updateInputValue}/>
+              <input type='text' name='element' placeholder='Enter a new task' value={this.state.inputValue} onChange={this.updateInputValue} />
             </label>
-            <button type="button" onClick={this.addItem}> Add </button>
+            <button type='button' onClick={this.addItem}> Add </button>
           </form>
-          <TodoElements entries={this.state.items} remove={this.removeItem}/>
+          <TodoElements entries={this.state.items} remove={this.removeItem} />
         </div>
       </div>
-    );
+    )
   }
 
+})
 
-});
-
-export default TodoList;
+export default TodoList
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import TodoList from './TodoList.js';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import TodoList from './TodoList.js'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<TodoList />, document.getElementById('input'));
+ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<TodoList />, document.getElementById('input'))
